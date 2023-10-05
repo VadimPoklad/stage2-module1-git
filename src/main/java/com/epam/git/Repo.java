45e5d1d89bd72db79c1f115b78ae1a51f36af7cd
Vadym Poklad <vadik.pokladd@gmail.com>
2023-10-05 13:38:13 +0300

    Answ

--- src/main/java/com/epam/git/Repo.java
+++ src/main/java/com/epam/git/Repo.java
@@ -1,5 +1,5 @@
 package com.epam.git;
 
 public class Repo {
-    public static String REPO_LINK = "https://github.com/username/reposotoryName";
+    public static String REPO_LINK = "https://github.com/VadimPoklad/stage2-module1-git-my";
 }
